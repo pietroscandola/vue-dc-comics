5b0e98159203b727a35e0f00d2a8e3f476ffa1a1
pietroscandola <pietro.scandolavr@gmail.com>
2022-02-08 15:31:48 +0100

    add header and img

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Header,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container{
+  max-width: 1200px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+      <header>
+          <nav>
+              <div class="row d-flex ">
+                  <div class="col-left">
+                      <figure>
+                          <img src="../assets/img/dc-logo.png" alt="Logo DC">
+                      </figure>
+                  </div>
+                  <div class="col-right">
+                      <ul class="d-flex ">
+                          <li class="active"><a href="#">CHARACTERS</a></li>
+                          <li><a href="#">COMICS</a></li>
+                          <li><a href="#">MOVIES</a></li>
+                          <li><a href="#">TV</a></li>
+                          <li><a href="#">GAMES</a></li>
+                          <li><a href="#">COLLECTIBLES</a></li>
+                          <li><a href="#">VIDEOS</a></li>
+                          <li><a href="#">FANS</a></li>
+                          <li><a href="#">NEWS</a></li>
+                          <li><a href="#">SHOP</a></li>                                                    
+                      </ul>
+                  </div>                
+              </div>
+          </nav>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.d-flex {
+    display: flex;
+}
+
+.row {
+    display: flex;
+    justify-content:space-between;
+}
+
+.col-right{
+    display:flex;
+    align-items: center;
+    justify-content: flex-end;
+}
+
+
+li{
+    margin: 0 15px;
+    list-style-type: none;
+    font-size:14px;
+    font-weight: bold;
+    padding: 20px 0;   
+}
+
+li a {
+   color: black;
+   text-decoration:none;
+   
+}
+
+.active {
+    color: dodgerblue;
+    border-bottom: 5px solid dodgerblue;    
+}
+
+</style>
